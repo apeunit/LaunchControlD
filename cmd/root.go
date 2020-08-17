331bc9d833ca313678359e15ea2e9023ae4ac7e7
Andrea Giacobino <no.andrea@gmail.com>
2020-08-17 18:16:44 +0200

    feat: add basic command setup
    Add logging configuraition, versioning support, etc

--- cmd/root.go
+++ cmd/root.go
@@ -16,82 +16,86 @@ limitations under the License.
 package cmd
 
 import (
-  "fmt"
-  "os"
-  "github.com/spf13/cobra"
+	"fmt"
+	"os"
 
-  homedir "github.com/mitchellh/go-homedir"
-  "github.com/spf13/viper"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
 
+	homedir "github.com/mitchellh/go-homedir"
+	"github.com/spf13/viper"
 )
-
 
 var cfgFile string
 
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-  Use:   "evtvzd",
-  Short: "A brief description of your application",
-  Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-  // Uncomment the following line if your bare application
-  // has an action associated with it:
-  //	Run: func(cmd *cobra.Command, args []string) { },
+	Use:   "evtvzd",
+	Short: "The Eventivize command & control service",
+	Long:  ``,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	//	Run: func(cmd *cobra.Command, args []string) { },
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
-func Execute() {
-  if err := rootCmd.Execute(); err != nil {
-    fmt.Println(err)
-    os.Exit(1)
-  }
+func Execute(version string) {
+	rootCmd.Version = version
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
+
+var debug bool
 
 func init() {
-  cobra.OnInitialize(initConfig)
+	cobra.OnInitialize(initConfig)
 
-  // Here you will define your flags and configuration settings.
-  // Cobra supports persistent flags, which, if defined here,
-  // will be global for your application.
-
-  rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.evtvzd.yaml)")
-
-
-  // Cobra also supports local flags, which will only run
-  // when this action is called directly.
-  rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// config file
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.evtvzd.yaml)")
+	// verbose logging
+	rootCmd.PersistentFlags().BoolVarP(&debug, "debug", "d", false, "Enable debug logging")
 }
-
 
 // initConfig reads in config file and ENV variables if set.
 func initConfig() {
-  if cfgFile != "" {
-    // Use config file from the flag.
-    viper.SetConfigFile(cfgFile)
-  } else {
-    // Find home directory.
-    home, err := homedir.Dir()
-    if err != nil {
-      fmt.Println(err)
-      os.Exit(1)
-    }
+	if debug {
+		// log.SetReportCaller(true)
+		// Output to stdout instead of the default stderr
+		// Can be any io.Writer, see below for File example
+		// log.SetOutput(os.Stdout)
 
-    // Search config in home directory with name ".evtvzd" (without extension).
-    viper.AddConfigPath(home)
-    viper.SetConfigName(".evtvzd")
-  }
+		// Only log the warning severity or above.
+		log.SetLevel(log.DebugLevel)
+	}
+	fmt.Printf(`
+┌─┐┬  ┬┌┬┐┬  ┬┌─┐╔╦╗
+├┤ └┐┌┘ │ └┐┌┘┌─┘ ║║
+└─┘ └┘  ┴  └┘ └─┘═╩╝ v%s\n
+  `, rootCmd.Version)
 
-  viper.AutomaticEnv() // read in environment variables that match
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := homedir.Dir()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
 
-  // If a config file is found, read it in.
-  if err := viper.ReadInConfig(); err == nil {
-    fmt.Println("Using config file:", viper.ConfigFileUsed())
-  }
+		// Search config in home directory with name ".evtvzd" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".evtvzd")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
 }
-
