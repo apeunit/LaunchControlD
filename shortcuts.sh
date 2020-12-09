b862b4cb493bb1909b30e6c413d9bc6f26d64bbc
Andrew Chiw <randomshinichi4869@gmail.com>
2020-12-09 14:58:43 +0100

    expanded integration test

--- shortcuts.sh
+++ shortcuts.sh
@@ -8,6 +8,8 @@ alias quickssh='docker-machine -s /tmp/workspace/evts/$EVTID/.docker/machine ssh
 function reenv() {
     export EVTID=$1
     export EVTDIR=/tmp/workspace/evts/$EVTID
+    export EVT=$EVTDIR/event.json
+    export EVTIP=`jq -r '.state."alice@apeunit.com".Instance.IPAddress' $EVT`
     export MACHINE_STORAGE_PATH=$EVTDIR/.docker/machine/
 }
 
@@ -18,16 +20,31 @@ function testsendworks() {
     launchpayloadcli tx send $FAUCET $ALICE 1drop --keyring-backend test --home /tmp/workspace/evts/$EVTID/nodeconfig/extra_accounts/dropgiver/ --node tcp://192.168.99.100:26657 --chain-id $EVTID
 }
 
-function faucetstatus() {
-    export EVT=$EVTDIR/event.json
-    export FAUCETIP=`jq -r '.state."alice@apeunit.com".Instance.IPAddress' $EVT`
-    curl http://$FAUCETIP:8000/status
-}
+function testeverything {
+    echo "FAUCET: GET http://$EVTIP:8000/status"
+    curl http://$EVTIP:8000/status > /dev/null
+    echo ""
 
-function faucetsend() {
-    export EVT=$EVTDIR/event.json
-    export ALICE=`jq -r '.accounts."alice@apeunit.com".address' $EVT`
-    export FAUCET=`jq -r '.accounts.dropgiver.address' $EVT`
-    export FAUCETIP=`jq -r '.state."alice@apeunit.com".Instance.IPAddress' $EVT`
-    curl -v -X POST -d 'token=abadjoke' http://$FAUCETIP:8000/send/$ALICE/500drop
+    ALICE=`jq -r '.accounts."alice@apeunit.com".address' $EVT`
+    FAUCET=`jq -r '.accounts.dropgiver.address' $EVT`
+    echo "FAUCET: POST http://$EVTIP:8000/send/$ALICE/500drop"
+    TXHASH=`curl -X POST -d 'token=abadjoke' http://$EVTIP:8000/send/$ALICE/1drops |jq -r '.txhash'`
+    echo ""
+
+    echo "GET $EVTIP:1317/auth/accounts/$ALICE"
+    curl http://$EVTIP:1317/auth/accounts/$ALICE
+    echo ""
+
+    echo "GET $EVTIP:1317/bank/balances/$ALICE"
+    curl http://$EVTIP:1317/bank/balances/$ALICE
+    echo ""
+
+    sleep 0.5
+
+    echo "GET $EVTIP:1317/txs"
+    curl http://$EVTIP:1317/txs
+    echo ""
+
+    echo "GET $EVTIP:1317/txs/$TXHASH"
+    curl http://$EVTIP:1317/txs/$TXHASH
 }
