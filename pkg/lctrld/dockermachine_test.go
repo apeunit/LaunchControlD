3ab6485a717fe22d2647eaecbacaf85b4a72f15f
Andrew Chiw <randomshinichi4869@gmail.com>
2021-01-26 00:09:40 +0100

    deleted TestDockerMachineConfig

--- pkg/lctrld/dockermachine_test.go
+++ pkg/lctrld/dockermachine_test.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/apeunit/LaunchControlD/pkg/cmdrunner"
 	"github.com/apeunit/LaunchControlD/pkg/config"
-	"github.com/apeunit/LaunchControlD/pkg/model"
 	"github.com/apeunit/LaunchControlD/pkg/utils"
 	"github.com/stretchr/testify/assert"
 )
@@ -29,29 +28,6 @@ var mockSettings = config.Schema{
 		},
 		Env: []string{"VIRTUALBOX_BOOT2DOCKER_URL=/home/shinichi/boot2docker.iso"},
 	},
-}
-
-func TestDockerMachineConfig(t *testing.T) {
-	dmc := NewDockerMachine(mockSettings, "drop-28b10d4eff415a7b0b2c")
-	assert.Equal(t, "testdata/evts/drop-28b10d4eff415a7b0b2c/.docker/machine/machines/drop-28b10d4eff415a7b0b2c-0", dmc.HomeDir("0"))
-
-	mc, err := dmc.ReadConfig("0")
-	assert.Nil(t, err)
-	mcExpected := &model.Machine{
-		N:                "0",
-		EventID:          "drop-28b10d4eff415a7b0b2c",
-		DriverName:       "",
-		TendermintNodeID: "",
-		Instance: model.MachineNetworkConfig{
-			IPAddress:   "192.168.99.100",
-			MachineName: "drop-28b10d4eff415a7b0b2c-0",
-			SSHUser:     "docker",
-			SSHPort:     36027,
-			SSHKeyPath:  "/tmp/workspace/evts/drop-28b10d4eff415a7b0b2c/.docker/machine/machines/drop-28b10d4eff415a7b0b2c-0/id_rsa",
-			StorePath:   "/tmp/workspace/evts/drop-28b10d4eff415a7b0b2c/.docker/machine",
-		},
-	}
-	assert.Equal(t, mcExpected, mc)
 }
 
 func TestDockerMachineProvisionMachine(t *testing.T) {
