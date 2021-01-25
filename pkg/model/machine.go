83c29d5c8e3ba2f7b4c19c262c259f8692f0150a
Andrew Chiw <randomshinichi4869@gmail.com>
2021-01-25 19:12:18 +0100

    moved Machine to package model, Machine.Run(), Machine.Copy()

---
+++ pkg/model/machine.go
@@ -0,0 +1,72 @@
+package model
+
+import (
+	"fmt"
+	"path/filepath"
+	"strings"
+
+	"github.com/apeunit/LaunchControlD/pkg/cmdrunner"
+	"github.com/apeunit/LaunchControlD/pkg/config"
+)
+
+// Machine holds the configuration of a Machine
+type Machine struct {
+	N                string               `json:"N"`
+	EventID          string               `json:"EventID"`
+	DriverName       string               `json:"DriverName"`
+	TendermintNodeID string               `json:"TendermintNodeID"`
+	Instance         MachineNetworkConfig `json:"Instance"`
+	settings         config.Schema
+	dockerMachineEnv []string
+	CmdRunner        cmdrunner.CommandRunner
+}
+
+// ID joins the EventID and N, e.g. EventID is evtx-d97517a3673688070aef, N is
+// 1, then it will return evtx-d97517a3673688070aef-1
+func (m *Machine) ID() string {
+	s := []string{m.EventID, m.N}
+	return strings.Join(s, "-")
+}
+
+// HomeDir returns the path of a docker-machine instance home, e.g.
+// /tmp/workspace/evts/drop-xxx/.docker/machine/machines/drop-xxx-0/
+// TODO: reconcile with duplicate logic from lctrld/common.go
+func (m *Machine) HomeDir() string {
+	return filepath.Join(m.settings.Workspace, "evts", m.EventID, ".docker", "machine", "machines", fmt.Sprintf("%s-%s", m.EventID, m.N))
+}
+
+// Run tells docker-machine to run a command within this provisioned Machine
+func (m *Machine) Run(cmd []string) (err error) {
+	envVars := append(m.dockerMachineEnv,
+		"DOCKER_TLS_VERIFY=1",
+		fmt.Sprintf("DOCKER_HOST=tcp://%s:2376", m.Instance.IPAddress),
+		fmt.Sprintf("DOCKER_CERT_PATH=%s", m.HomeDir()),
+		fmt.Sprintf("DOCKER_MACHINE_NAME=%s", m.ID()),
+	)
+
+	_, err = m.CmdRunner(cmd, envVars)
+	return
+}
+
+// Copy recursively copies a path from the local machine to the provisioned Machine
+// TODO: reconcile with duplicate logic from lctrld/common.go: is dmBin() needed at all?
+func (m *Machine) Copy(sourcePath, destPath string) (err error) {
+	p := []string{"docker-machine", "scp", "-r", sourcePath, fmt.Sprintf("%s:%s", m.ID(), destPath)}
+	_, err = m.CmdRunner(p, m.dockerMachineEnv)
+	return
+}
+
+// TendermintPeerNodeID returns <nodeID@192.168.1....:26656> which is used in specifying peers to connect to in the daemon's config.toml file
+func (m *Machine) TendermintPeerNodeID() string {
+	return fmt.Sprintf("%s@%s:26656", m.TendermintNodeID, m.Instance.IPAddress)
+}
+
+// MachineNetworkConfig holds information read from docker-machine about the deployed VM's network settings
+type MachineNetworkConfig struct {
+	IPAddress   string `json:"IPAddress"`
+	MachineName string `json:"MachineName"`
+	SSHUser     string `json:"SSHUser"`
+	SSHPort     int    `json:"SSHPort"`
+	SSHKeyPath  string `json:"SSHKeyPath"`
+	StorePath   string `json:"StorePath"`
+}
